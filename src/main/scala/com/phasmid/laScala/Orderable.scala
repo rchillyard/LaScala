8c92a6d15d2cdf8a39672bc2e19df28fa721a55a
Harinath Sonnathi <hsonnathi@gmail.com>
2016-07-13 16:48:05 -0400

    added ScalaTests for Orderable class

--- src/main/scala/com/phasmid/laScala/Orderable.scala
+++ src/main/scala/com/phasmid/laScala/Orderable.scala
@@ -56,67 +56,6 @@ object Orderable {
 
     def formatter(s: String) = DateTimeFormatter.ofPattern(s)
   }
-
-  implicit object OrderableIntInt extends Orderable[(Int, Int)] {
-    def unit(x: (Int, Int)) = x
-
-    val sTuple = """\((\d+),(\d+)\)|(\d+),(\d+)""".r
-
-    def fromString(s: String)(implicit pattern: String = "") = s match {
-      case sTuple(x, y, _, _) => Try(x.toInt, y.toInt)
-      case sTuple(_, _, x, y) => Try(x.toInt, y.toInt)
-    }
-
-    def viaLookup(s: String, f: String => Option[(Int, Int)]) = optionToTry(f(s), new OrderableException(s"$s is not defined"))
-
-    def zero = (0, 0)
-
-    /**
-      * Compare tuples x and y where x is most significant
-      *
-      * @param x the most significant comparand
-      * @param y the least significant comparand
-      * @return 0, -1 or +1 as appropriate
-      */
-    def compare(x: (Int, Int), y: (Int, Int)): Int = {
-      val cf = x._1.compare(y._1)
-      if (cf == 0) x._2.compare(y._2)
-      else cf
-    }
-  }
-
-  implicit object OrderableIntIntInt extends Orderable[(Int, Int, Int)] {
-    def unit(x: (Int, Int, Int)) = x
-
-    val sTuple = """\((\d+),(\d+),(\d+)\)|(\d+),(\d+),(\d+)""".r
-
-    def fromString(s: String)(implicit pattern: String = "") = s match {
-      case sTuple(x, y, z, _, _, _) => Try(x.toInt, y.toInt, z.toInt)
-      case sTuple(_, _, _, x, y, z) => Try(x.toInt, y.toInt, z.toInt)
-    }
-
-    def zero = (0, 0, 0)
-
-    def viaLookup(s: String, f: (String) => Option[(Int, Int, Int)]): Try[(Int, Int, Int)] = optionToTry(f(s), new OrderableException(s"$s is not defined"))
-
-    /**
-      * Compare tuples x and y where x is most significant
-      *
-      * @param x the most significant comparand
-      * @param y the least significant comparand
-      * @return 0, -1 or +1 as appropriate
-      */
-    def compare(x: (Int, Int, Int), y: (Int, Int, Int)): Int = {
-      val cf = x._1.compare(y._1)
-      if (cf == 0) {
-        val cf = x._2.compare(y._2)
-        if (cf == 0) x._3.compare(y._3)
-        else cf
-      }
-      else cf
-    }
-  }
-
   class OrderableException(s: String) extends Exception(s)
 
 }
--- src/test/scala/com/phasmid/laScala/OrderableSpec.scala
+++ src/test/scala/com/phasmid/laScala/OrderableSpec.scala
@@ -1,5 +1,7 @@
 package com.phasmid.laScala
 
+import java.time.LocalDate
+
 import com.phasmid.laScala.Orderable.{OrderableDate, OrderableString}
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -14,6 +16,30 @@ class OrderableSpec extends FlatSpec with Matchers {
     val dt = OrderableString.unit("2016-01-01")
     dt shouldBe "2016-01-01"
   }
+  "zero string" should "result in OrderableString of length zero" in {
+    implicit val pattern = ""
+    val dt = OrderableString zero
+  }
+  "string1" should  "be the string string1" in {
+    OrderableString compare("string", "string") shouldBe (0)
+  }
+  "string" should "be the string from string" in {
+    implicit val pattern = ""
+    val st = OrderableString.fromString("String1")
+    st should matchPattern { case Success("String1") => }
+  }
+  "01/01/2016" should "result in string using via" in {
+    val m: Map[String, String] = Map("k" -> "v")
+    implicit val pattern = "MM/dd/uuuu"
+    val st = OrderableString.viaLookup("k", m get)
+    st should matchPattern { case Success("v") => }
+  }
+  "2016-01-01" should "result in Orerabledate" in {
+    implicit val pattern = ""
+    val dt = LocalDate of (2016, 2, 1)
+    val dt2 = OrderableDate.unit(dt)
+    dt2 shouldBe dt
+  }
   "2016-01-01" should "result in date" in {
     implicit val pattern = ""
     val dt = OrderableDate.fromString("2016-01-01")
@@ -23,5 +49,23 @@ class OrderableSpec extends FlatSpec with Matchers {
     implicit val pattern = "MM/dd/uuuu"
     val dt = OrderableDate.fromString("01/01/2016")
     dt should matchPattern { case Success(_) => }
+  }
+  "01/01/2016" should "result in LocalDate using via" in {
+    val dt = LocalDate of (2016, 2, 1)
+    val m: Map[String, LocalDate] = Map("k" -> dt)
+    implicit val pattern = "MM/dd/uuuu"
+    val dt2 = OrderableDate.viaLookup("k", m get _ )
+    dt2 should matchPattern { case Success(dt) => }
+  }
+  "zero" should "result in date now" in {
+    implicit val pattern = ""
+    val dt1 = OrderableDate zero
+    val dt2 = LocalDate.now()
+    dt1 shouldBe dt2
+  }
+  "compare LocalDates 2016/02/01 2016/02/01 " should "result zero" in {
+    val dt1 = LocalDate of (2016, 2, 1)
+    val dt2 = LocalDate of (2016, 2, 1)
+    OrderableDate compare (dt1, dt2) shouldBe (0)
   }
 }
